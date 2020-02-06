02a2cfad92c6b7d33295383d1f7a39f7f932c1c6
Mikkel Flindt Heisterberg <lekkim@heisterberg.dk>
2020-02-06 07:47:20 +0000

    Minor changes that were lingering

--- scripts/backup/backup.sh
+++ scripts/backup/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-JSON=`curl -s -XPOST http://localhost:9090/api/v1/admin/tsdb/snapshot?skip_head=true`
+JSON=`curl -s -XPOST http://localhost:9090/api/v1/admin/tsdb/snapshot?skip_head=false`
 NAME=`echo $JSON | jq -r ".data.name"`
 TIMESTAMP=`date "+%Y%m%dT%H%M%S"`
 FILENAME=/tmp/sensorcentral-prometheus-grafana-backup-$TIMESTAMP.tar.gz
